b2ef89322705c28003da9e765a0cf3e7b8f0c7f3
Vale <valetolpegin@gmail.com>
2014-09-10 13:33:03 -0400

    Update Main.c

--- Autonomous/Main.c
+++ Autonomous/Main.c
@@ -1,1 +1,16 @@
 //Put main code for Autonomous Program
+
+void MoveRobotForward()
+{
+  motor[leftRobotMotor] = 75;
+  motor[rightRobotMotor] = 75;
+}
+
+void MoveRobotForward( int timeToMove )
+{
+  motor[leftRobotMotor] = 75;
+  motor[rightRobotMotor] = 75;
+  wait1Msec( timeToMove );
+  motor[leftRobotMotor] = 0;
+  motor[rightRobotMotor] = 0;
+}
