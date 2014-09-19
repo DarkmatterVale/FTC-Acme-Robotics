eb1e8fbfb44bd39628b37186a54057e208a0f6ab
Vale <valetolpegin@gmail.com>
2014-09-19 10:10:50 -0400

    Added some psuedocode to main.c

--- Teleop/MainTeleop.c
+++ Teleop/MainTeleop.c
@@ -2,5 +2,26 @@
 
 task main()
 {
-  //here, put all the code for getting joystick settings ( getjoysticksettings() ), moving the robot when a button is pressed
+  //here, put all the code for getting joystick settings ( getjoysticksettings() ), moving the robot when a button is pressed, etc
+  
+  //while loop ( forever )
+    //getjoysticksettings()
+    
+    //if joystick controls ( y1 & y2 ) have high values
+      //go forward at full power
+    //else if joystick controls ( y1 & y2 ) do not have high values
+      //go backward
+      
+    //getjoysticksettings
+    
+    //if joystick button ( button that will use tower lift ) is pressed
+      //move tower lift upward
+    //else if joystick button ( button to launch catapult ) is pressed
+      //launch catapult
+    //else if button ( button to lock rolling goal to robot ) is pressed
+      //lock goal to robot
+    //else if button ( button to release goal ) is pressed
+      //release goal
+      
+  //end while loop
 }
