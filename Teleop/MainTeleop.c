fdbb43c7dc749675aa5e5ecdc89c97a87d9d12c1
Vale <valetolpegin@gmail.com>
2014-09-19 07:59:04 -0400

    added main() task

--- Teleop/MainTeleop.c
+++ Teleop/MainTeleop.c
@@ -1,1 +1,6 @@
 //Put main file code for Teleop Program
+
+task main()
+{
+  //here, put all the code for getting joystick settings ( getjoysticksettings() ), moving the robot when a button is pressed
+}
