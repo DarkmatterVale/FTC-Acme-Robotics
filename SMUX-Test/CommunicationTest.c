8a8c0614ab5d5331ec846bccfb3927add20173bd
Vale <valetolpegin@gmail.com>
2014-11-05 12:53:39 -0500

    Added HTSMUXhalt() method

--- SMUX-Test/CommunicationTest.c
+++ SMUX-Test/CommunicationTest.c
@@ -1,1 +1,19 @@
 //Test to just prove communication between NXT and SMUX
+
+void HTSMUXhalt( tSensors smux )
+{
+  //Creating message to send
+  ubyte sendMsg[4];
+  
+  //Assigning message info
+  sendMsg[0] = 3;
+  sendMsg[1] = 0x10;
+  sendMsg[2] = 0x20;
+  sendMsg[3] = 0;
+  
+  //Sending message
+  sendI2CMsg( smux, sendMsg, 0 );
+  
+  //Required wait time
+  wait1Msec( 50 );
+}
