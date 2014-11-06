417b3c6a62c161caa37508adfe85b07262e36ac6
Vale <valetolpegin@gmail.com>
2014-11-06 15:59:04 -0500

    added SMUX initialization

--- Autonomous/Parking-Zone-Start/MainAutonomous.c
+++ Autonomous/Parking-Zone-Start/MainAutonomous.c
@@ -1,5 +1,56 @@
 //File containing instructions for parking zone start
 
+void HTSMUXhalt( tSensors smux )
+{
+  //Creating message to send
+  ubyte sendMsg[4];
+  
+  //Assigning message info
+  sendMsg[0] = 3;
+  sendMsg[1] = 0x10;
+  sendMsg[2] = 0x20;
+  sendMsg[3] = 0;
+  
+  //Sending message
+  sendI2CMsg( smux, sendMsg, 0 );
+  
+  //Required wait time
+  wait1Msec( 50 );
+}
+
+void HTSMUXautodetect( tSensors smux )
+{
+  //Creating message to send
+  ubyte sendMsg[4];
+  
+  //Assigning message info
+  sendMsg[0] = 3;
+  sendMsg[1] = 0x10;
+  sendMsg[2] = 0x20;
+  sendMsg[3] = 1;
+  
+  //Sending message
+  sendI2CMsg( smux, sendMsg, 0 );
+  
+  //Required wait time
+  wait1Msec( 500 );
+}
+
+void HTSMUXrun( tSensors smux )
+{
+  //Creating message to send
+  ubyte sendMsg[4];
+  
+  //Assigning message info
+  sendMsg[0] = 3;
+  sendMsg[1] = 0x10;
+  sendMsg[2] = 0x20;
+  sendMsg[3] = 2;
+  
+  //Send message
+  sendI2CMsg( smux, sendMsg, 0 );
+}
+
 void moveRobotForward( int speed )
 {
   /*
