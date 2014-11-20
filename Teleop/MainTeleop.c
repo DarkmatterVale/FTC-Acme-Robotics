e3044a9049d544f901b020a45e476450402a3742
Vale <valetolpegin@gmail.com>
2014-11-20 09:13:38 -0500

    Update MainTeleop.c

--- Teleop/MainTeleop.c
+++ Teleop/MainTeleop.c
@@ -1,5 +1,12 @@
 //Add config variables here
 
+/*
+
+  Joystick 1 will move the robot's drive motors
+  Joystick 2 will move the robot's lift
+
+*/
+
 #include "JoystickDriver.c";
 
 task main()
@@ -10,20 +17,36 @@ task main()
     //getjoysticksettings()
     getJoystickSettings( joystick );
     
-    motor[ leftDriveMotor ] = ( int ) pow( 0.006( joystick.joy1_y1 ), 2 ) - 1;
-    motor[ rightDriveMotor ] = ( int ) pow( 0.006( joystick.joy1_y2 ), 2 ) - 1;
+    //set the drive motors
+    motor[ leftDriveMotor ]  = ( int ) pow( 0.006 * joystick.joy1_y1, 2 ) - 1;
+    motor[ rightDriveMotor ] = ( int ) pow( 0.006 * joystick.joy1_y2, 2 ) - 1;
       
     //getjoysticksettings
     getJoystickSettings( joystick );
     
-    //if joystick button ( button that will use tower lift ) is pressed
-      //move tower lift upward
-    //else if joystick button ( button to launch catapult ) is pressed
-      //launch catapult
-    //else if button ( button to lock rolling goal to robot ) is pressed
-      //lock goal to robot
-    //else if button ( button to release goal ) is pressed
-      //release goal
+    //if joystick button ( button that will use tower lift to go up) is pressed
+    if ( )
+    {
+      //move tower lift up
+    }
+    
+    //if joystick button ( button that will use tower lift to go down) is pressed
+    else if ( )
+    {
+      //move tower lift down
+    }
       
-  //end while loop
+    //if button ( button to lock rolling goal to robot ) is pressed
+    if ( )
+    {
+      //lock goal to robot
+    }
+      
+    //else if button ( button to release goal ) is pressed
+    else if ( )
+    {
+      //release goal
+    }
+      
+  } //end while loop
 }
