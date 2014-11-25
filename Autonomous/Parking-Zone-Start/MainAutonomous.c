f3e3ab12739b7a2a2f5717d387bc0e543a6cddfc
Vale <valetolpegin@gmail.com>
2014-11-25 13:35:26 -0500

    Added base code
    Copy of other autonomous program

--- Autonomous/Parking-Zone-Start/MainAutonomous.c
+++ Autonomous/Parking-Zone-Start/MainAutonomous.c
@@ -10,7 +10,10 @@
 #pragma config(Servo,  srvo_S1_C3_3,    servo3,               tServoNone)
 #pragma config(Servo,  srvo_S1_C3_4,    servo4,               tServoNone)
 #pragma config(Servo,  srvo_S1_C3_5,    servo5,               tServoNone)
-#pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)r
+#pragma config(Servo,  srvo_S1_C3_6,    servo6,               tServoNone)
+
+#pragma config(Sensor, S2,     HTSMUX,              sensorLowSpeed)
+#pragma config(Sensor, S3,     HTSMUX,              sensorLowSpeed)
 	//Sensors:
   		//lightSensor ( two at the front of the robot )
   		//lightSensor 2
@@ -22,13 +25,31 @@
   		//Compass sensor
   
 //Defines
-#define FORWARD_STANDARD 60
+//motors
+#define FORWARD_STANDARD 70
 #define FORWARD_FULL 100
-
 #define STOP 0
-
-#define BACKWARD_STANDARD 60
+#define BACKWARD_STANDARD 70
 #define BACKWARD_FULL 100
+
+//lift
+#define LIFT_POSITION_0 0
+#define LIFT_POSITION_1 100
+#define LIFT_POSITION_2 200
+#define LIFT_POSITION_3 300
+#define LIFT_POSITION_4 400
+
+//servo
+#define SERVO_DOWN 180
+#define SERVO_UP 0
+
+//Includes
+#include "drivers/hitechnic-sensormux.h"
+#include "drivers/hitechnic-colour-v2.h"
+#include "drivers/hitechnic-compass.h"
+#include "drivers/hitechnic-gyro.h"
+#include "drivers/hitechnic-irseeker-v2.h"
+#include "drivers/lego-ultrasound.h"
   
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -52,16 +73,10 @@ Author(s):
 	
 */
 
-//GLOBAL VARIABLES
+//Global variables' declarations
 int matValue =     0;
 int surfaceValue = 0;
 int rampValue =    0;
-	
-/*
-To Do list:
-	- Move manipulator 1 method needs to be overloaded
-
-*/
 
 void haltRobot()
 {
@@ -377,7 +392,272 @@ void moveRobotRight( int speed, int encoderValue )
   motor[ rightDriveMotor ] = 0;
 }
 
+void findThreshHold()
+{
+  /*
+  Rev 2.0
+  In this function, we need to set the threshhold(s) for the light sensor and tell it the values of all of the lines on the board
+  
+  Inputs:
+  	color sensor
+  	orange button on NXT
+  	
+  Outputs:
+  	Variables' values
+  
+  To Do:
+  	NONE
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  //We need to get values for:
+    //ramp
+    //ground tape
+    //Normal ground ( grey mat )
+    
+  //Get ramp value
+  nxtDisplayCenteredTextLine( 0, "Place light sensor over " );
+  nxtDisplayCenteredTextLine( 1, "the red ramp, then " );
+  nxtDisplayCenteredTextLine( 2, "push the orange button" );
+
+  while ( !( nNxtButtonPressed == 3 ) )
+  {
+	nxtDisplayCenteredTextLine( 3, " light value: %d", SensorValue[ lightSensor ] );
+  }
+
+  rampValue = SensorValue[ lightSensor ];
+  
+  //Get surface value
+  nxtDisplayCenteredTextLine( 0, "Place light sensor over " );
+  nxtDisplayCenteredTextLine( 1, "red surface line, then " );
+  nxtDisplayCenteredTextLine( 2, "push the orange button" );
+
+  while ( !( nNxtButtonPressed == 3 ) )
+  {
+	nxtDisplayCenteredTextLine( 3, "light value: %d", SensorValue[ lightSensor ] );
+  }
+
+  surfaceValue = SensorValue[ lightSensor ];
+  
+  //Get grey mat value
+  nxtDisplayCenteredTextLine( 0, "Place light sensor over " );
+  nxtDisplayCenteredTextLine( 1, "gray mat, then " );
+  nxtDisplayCenteredTextLine( 2, "push the orange button" );
+
+  while ( !( nNxtButtonPressed == 3 ) )
+  {
+	nxtDisplayCenteredTextLine( 3, "light value: %d", SensorValue[ lightSensor ] );
+  }
+
+  matValue = SensorValue[ lightSensor ];
+}
+
+void initializeRobot()
+{
+  /*
+  Rev 1.4
+  In here, we should test all of the motors/moving parts of the robot by testing them for a couple of seconds, and then resetting the parts
+
+  Inputs:
+  	servos
+  	sensors
+  Outputs:
+  	servos
+  	motors
+  
+  To Do:
+  	NONE
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  
+  //Find/Set threshhold for light sensor by calling findThreshHold()
+  findThreshHold();
+  
+  //Test main motors
+  motor[ leftDriveMotor ]  = 10;
+  motor[ rightDriveMotor ] = 10;
+  wait1Msec( 1000 );
+  
+  haltRobot();
+  
+  //Test lift
+  motor[ liftMotor ] = 70;
+  wait1Msec( 2000 );
+  
+  haltRobot();
+        
+  //Move ball container around
+  servo[ servo1 ] = 180;
+  wait1Msec( 1000 );
+  servo[ servo1 ] = 0;
+}
+
+void moveManipulator( int encoderValue )
+{
+  /*
+  Rev 1.0
+  In here, we need the "tower" to go up and drop the balls into the rolling ball "goal"
+  
+  Inputs:
+  	Passed encoderValue variable
+  	liftMotor encoder value
+  Outputs:
+  	liftMotor
+  
+  To Do:
+  	NONE
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  //reset encoder
+  nMotorEncoder[ liftMotor ] = 0;
+  
+  //start lift motor
+  motor[liftMotor] = FORWARD_STANDARD;
+  
+  if ( encoderValue > 0 )
+  {
+  	//while lift has not reached wanted position
+  	while ( nMotorEncoder[liftMotor] < encoderValue )
+  	{
+  	}
+  	
+  	motor[ liftMotor ] = STOP;
+  } else if ( encoderValue < 0 )
+  {
+  	//while lift has not reached wanted position
+  	while ( nMotorEncoder[liftMotor] > encoderValue )
+  	{
+  	}
+  	
+  	motor[ liftMotor ] = STOP;
+  }
+}
+
+void moveManipulator( int encoderValue, int speed )
+{
+  /*
+  Rev 1.0
+  In here, we need the "tower" to go up and drop the balls into the rolling ball "goal"
+  
+  Inputs:
+  	Passed encoderValue variable
+  	Passed speed variable
+  	liftMotor encoder value
+  Outputs:
+  	liftMotor
+  
+  To Do:
+	NONE
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  
+  //reset encoder
+  nMotorEncoder[ liftMotor ] = 0;
+  
+  //start lift motor
+  motor[liftMotor] = speed;
+  
+  if ( encoderValue > 0 )
+  {
+  	//while lift has not reached wanted position
+  	while ( nMotorEncoder[liftMotor] < encoderValue )
+  	{
+  	}
+  	
+  	motor[ liftMotor ] = STOP;
+  } else if ( encoderValue < 0 )
+  {
+  	//while lift has not reached wanted position
+  	while ( nMotorEncoder[liftMotor] > encoderValue )
+  	{
+  	}
+  	
+  	motor[ liftMotor ] = STOP;
+  }
+}
+
+void dragGoalsIntoScoringZone() //function for dragging goals into scoring zone
+{
+  /*
+  Rev 1.0
+  In here, we will need to have the robot push the ball holders/rollers into the box
+  
+  Inputs:
+  	IR sensor
+  	Compass sensor
+  Outputs:
+  	functions invoked
+  
+  To Do:
+  	Change method name more appropriate for procedure function
+  	Edit and be more detailed in Inputs and Outputs area
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  //for ( int i = 0; i < 3; i++ )
+  //{
+    //find goals
+      //find current position
+        //find IR beacon
+      //move towards goal
+      //turn around
+  
+    //grab goal
+      //drop post
+  
+    //drag to scoring zone
+      //move towards scoring zone
+  //}
+}
+
 task main()
 {
-	//Add main code here
+  /*
+  Rev 1.0
+  This is the main task which begins running once the competition starts
+  
+  To Do:
+  	Add code
+  
+  Author(s):
+  	Vale Tolpegin
+  */
+  
+  //Call InitializeRobot method
+  initializeRobot();
+  
+  //move toward goal(s)
+  moveRobotBackward( 70, 18720 );	
+  
+  //grab goal
+  servo[ servo1 ] = 0;
+  
+  //drop ball in goal
+  moveManipulator( 100, 90 );
+  
+  //turn the robot
+  moveRobotRight( 50, 800 );
+  
+  //move team goals to scoring area
+  while ( SensorValue[ SonarSensor ] > 20 )
+  {
+  	motor[ leftDriveMotor ]  = 70;
+  	motor[ rightDriveMotor ] = 70;
+  }
+  
+  //stop
+  haltRobot();
 }
